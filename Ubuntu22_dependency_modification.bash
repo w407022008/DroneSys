f3130fbd4a8e39e442b1ee56e0e4f4c59818df8d
w407022008 <w407022008@gmail.com>
2024-04-17 15:35:02 +0800

    upd for jammy_noetic_arm installation

---
+++ Ubuntu22_dependency_modification.bash
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+## Driver
+sed -i -e s/"-std=c++11"/"-std=c++17"/g ./Driver/realsense-ros-2.3.2/realsense2_camera/CMakeLists.txt
+sed -i -e s/"CMAKE_CXX_STANDARD 11"/"CMAKE_CXX_STANDARD 17"/g ./Driver/joystick_drivers/joy/CMakeLists.txt
+sed -i -e s/"CMAKE_CXX_STANDARD 11"/"CMAKE_CXX_STANDARD 17"/g ./Driver/joystick_drivers/spacenav_node/CMakeLists.txt
+sed -i -e s/"CMAKE_CXX_STANDARD 11"/"CMAKE_CXX_STANDARD 17"/g ./Driver/joystick_drivers/wiimote/CMakeLists.txt
+
+## Tools
+sed -i -e s/"-std=c++14"/"-std=c++17"/g ./Modules/tools/imu_cali/CMakeLists.txt
+
+## mavros_interface
+sed -i -e s/"-std=c++11"/"-std=c++17"/g ./Modules/mavros_interface/CMakeLists.txt
+
+## control
+sed -i -e s/"-std=c++11"/"-std=c++17"/g ./Modules/control/CMakeLists.txt
+
+## perception
+sed -i -e s/"-std=c++14"/"-std=c++17"/g ./Modules/perception/joy_remote/CMakeLists.txt
+sed -i -e s/"-std=c++11"/"-std=c++17"/g ./Modules/perception/OptiTrack/CMakeLists.txt
+sed -i -e s/"-std=c++11"/"-std=c++17"/g ./Modules/perception/sensors/CMakeLists.txt
+sed -i -e s/"-std=c++14"/"-std=c++17"/g ./Modules/perception/points_filter/CMakeLists.txt
+
+## Slam
+sed -i -e s/"-std=c++14"/"-std=c++17"/g ./Modules/slam/VINS-Fusion/loop_fusion/CMakeLists.txt
+sed -i -e s/"-std=c++14"/"-std=c++17"/g ./Modules/slam/VINS-Fusion/global_fusion/CMakeLists.txt
+sed -i -e s/"-std=c++11"/"-std=c++17"/g ./Modules/slam/orb_slam3_ros_wrapper/CMakeLists.txt
+sed -i -e s/"-std=c++14"/"-std=c++17"/g ./Modules/slam/VINS-Fusion/vins_estimator/CMakeLists.txt
+
+## planning
+sed -i -e s/"-std=c++14"/"-std=c++17"/g ./Modules/planning/planning_simulator/CMakeLists.txt
+sed -i -e s/"-std=c++14"/"-std=c++17"/g ./Modules/planning/globle_planner/CMakeLists.txt
+sed -i -e s/"-std=c++14"/"-std=c++17"/g ./Modules/planning/local_planner/CMakeLists.txt
+sed -i -e s/"-std=c++14"/"-std=c++17"/g ./Modules/planning/histo-planner/CMakeLists.txt
+sed -i -e s/"-std=c++11"/"-std=c++17"/g ./Modules/planning/ego-planner/CMakeLists.txt
+sed -i -e s/"mat_jerk(i, j)"/"mat_jerk((int)i, (int)j)"/g ./Modules/planning/ego-planner/include/polynomial_traj.h
+sed -i -e s/"-std=c++14"/"-std=c++17"/g ./Modules/planning/FastPlanner/CMakeLists.txt
+sed -i '65i add_compile_options(-std=c++17)' ./Modules/planning/FastPlanner/ThirdParty/sdf_tools/CMakeLists.txt
+sed -i -e s/"-std=c++0x"/"-std=c++17"/g ./Modules/planning/FastPlanner/ThirdParty/sdf_tools/CMakeLists.txt
+sed -i '5i #include <stdlib.h>' ./Modules/planning/FastPlanner/ThirdParty/arc_utilities/include/arc_utilities/first_order_deformation.h
+sed -i '16,19d' ./Modules/planning/local_planner/src/geo_guide_apf.cpp
+
--- compile__.sh
+++ compile__.sh
@@ -1,11 +1,14 @@
-catkin_make --source Driver --build build/Driver
+catkin_make --source Driver/realsense-ros-2.3.2 --build build/Driver/realsense-ros-2.3.2
 #catkin_make --source Driver/realsense-ros-2.3.2 --build build/Driver/realsense-ros -DCATKIN_ENABLE_TESTING=False -DCMAKE_BUILD_TYPE=Release
 catkin_make --source Modules/msgs --build build/msgs
 catkin_make --source Modules/common --build build/common_util
-catkin_make --source Modules/tools --build build/tools
+catkin_make --source Modules/tools/camera_models --build build/tools/camera_models
 catkin_make --source Experiment --build build/drone_experiment
 catkin_make --source Modules/mavros_interface --build build/mavros_interface
 catkin_make --source Modules/control --build build/control
 catkin_make --source Modules/perception --build build/perception
-catkin_make --source Modules/slam --build build/slam -j2
-catkin_make --source Modules/planning --build build/planning -j2
+catkin_make --source Modules/slam/VINS-Fusion --build build/slam/VINS-Fusion -j2
+#catkin_make --source Modules/planning/FastPlanner --build build/planning/FastPlanner -j2
+catkin_make --source Modules/planning/ego-planner --build build/planning/ego-planner -j2
+catkin_make --source Modules/planning/histo-planner --build build/planning/histo-planner -j2
+catkin_make --source Modules/planning/planning_simulator --build build/planning/planning_simulator -j2
